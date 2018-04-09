55ac5325aabb4d1379d73ec31ed49ad3bf4abefb
Jesus Aguilar <jabakabaka@gmail.com>
2018-04-09 13:46:44 -0500

    menu de navegación

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,13 +4,17 @@ import {Routes, RouterModule} from '@angular/router';
 //importar componentes
 import {EmpleadoComponent} from './empleado/empleado.component';
 import {FrutaComponent} from './fruta/fruta.component';
-
+import {HomeComponent} from './home/home.component';
+import {ContactoComponent} from './contacto/contacto.component';
 //coniguracion de rutas 
 const appRoutes: Routes=[
-    {path:'',component:EmpleadoComponent},
+    {path:'',component:HomeComponent},
     {path:'empleado',component:EmpleadoComponent},
     {path:'fruta',component:FrutaComponent},
-    {path:'**',component:EmpleadoComponent}
+    {path:'pagina-pricipal',component:HomeComponent},
+    {path:'contacto',component:ContactoComponent},
+
+    {path:'**',component:HomeComponent}//este es la ruta cinado falla
 ];
 //declaracion de array vacio
 export const appRoutingProviders: any[]=[];
